626910a30abeb9bec4a1ea1b3228998566a6883f
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-04-05 21:13:47 +0200

    Create socials navigation

---
+++ src/ui/SocialsNavigation.jsx
@@ -0,0 +1,13 @@
+import './socialsNavigation.scss';
+
+export default function SocialsNavigation() {
+    return (
+        <nav
+            className='socials-navigation'
+            aria-label='Socials'
+        >
+            <img src='/images/icon-facebook.svg' alt='Facebook page' />
+            <img src='/images/icon-twitter.svg' alt='Twitter page' />
+        </nav>
+    )
+}
