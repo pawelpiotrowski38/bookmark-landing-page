0c337ce11f1e19620690c2ea6ac1ad2204be7aa6
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2024-04-08 19:09:30 +0200

    Create icons

---
+++ src/ui/ArrowIcon.jsx
@@ -0,0 +1,7 @@
+export default function ArrowIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12">
+            <path fill="none" stroke="#5267DF" strokeWidth="3" d="M1 1l8 8 8-8"/>
+        </svg>
+    )
+}
